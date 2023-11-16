87b13ae109be32130167750c4c67b30deec45184
Trieu0207 <85786020+Trieu0207@users.noreply.github.com>
2023-11-16 12:25:40 +0700

    commit code

---
+++ backend_django/backend/lagu/urls.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register('testAPi', views.TestView, 'test')
+router.register("ban", views.BanViewSet, 'ban')
+router.register("menu", views.MenuViewSet, 'menu')
+router.register('dsDatBan', views.DsDatBanViewSet,'ds_dat_ban')
+router.register('thanhToan', views.HoaDonThanhToanViewSet, 'thanh_toan')
+router.register('khachHang', views.KhachHangViewSet, 'khach_hang')
+router.register("DsOrder", views.DsOrderViewSet, 'order')
+router.register("GiamGia", views.GiamGiaViewSet, 'voucher')
+router.register("chitiethoadon", views.ChiTietHoaDonViewSet, 'chitiethoadon')
+router.register('user', views.UserViewSet)
+router.register("xuatfile", views.ChangeFile, 'changefile')
+urlpatterns = [
+    path('', include(router.urls)),
+    # path('oauth2-info/', views.AuthInfo.as_view())
+]
